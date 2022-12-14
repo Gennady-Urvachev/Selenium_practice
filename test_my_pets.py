4a9f79fe5b252a470055a06ee2e6837a8f2a9383
Gennady.Urvachev <94367678+Gennady-Urvachev@users.noreply.github.com>
2022-12-14 16:16:34 +0200

    Add files via upload

---
+++ test_my_pets.py
@@ -0,0 +1,25 @@
+import pytest_selenium
+import pytest
+
+
+@pytest.fixture(autouse=True)
+
+def testing():
+   pytest.driver = webdriver.Chrome('/Users/ghena/chromedriver.exe')
+   # Переходим на страницу авторизации
+   pytest.driver.get('http://petfriends.skillfactory.ru/login')
+
+   yield
+
+   pytest.driver.quit()
+
+
+def test_show_my_pets():
+   # Вводим email
+   pytest.driver.find_element_by_id('email').send_keys('uniquemail@mail')
+   # Вводим пароль
+   pytest.driver.find_element_by_id('pass').send_keys('12345')
+   # Нажимаем на кнопку входа в аккаунт
+   pytest.driver.find_element_by_css_selector('button[type="submit"]').click()
+   # Проверяем, что мы оказались на главной странице пользователя
+   assert pytest.driver.find_element_by_tag_name('h1').text == "PetFriends"
